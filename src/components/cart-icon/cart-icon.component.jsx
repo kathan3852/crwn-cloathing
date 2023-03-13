1e54b20cff3ad6e383ad6b22f0d277e37e5d5707
Kathan Patel <kathan3852@gmail.com>
2023-03-13 23:49:08 +0530

    implemented add to cart function using redux

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import './cart-icon.styles.css'
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
 import { connect } from "react-redux";
-import toggleCartHidden from "../../redux/cart/cart.action";
+import {toggleCartHidden} from "../../redux/cart/cart.action";
 
 const CartIcon = ({toggleCartHidden}) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -7,8 +7,8 @@ const CollectionPreview = ({title,items}) => (
         <div className='title'>{title.toUpperCase()}</div>
         <div className='preview'>
             {
-                items.filter((item,idx) => idx<4).map(({id, ...otherProps}) => (
-                    <CollectionItem key={id} {...otherProps} />
+                items.filter((item,idx) => idx<4).map((item) => (
+                    <CollectionItem key={item.id} item={item} />
                 ))
             }
         </div>
